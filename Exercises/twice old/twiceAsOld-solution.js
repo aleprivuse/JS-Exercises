c8bbf2c9afa29060cea6e41c5d89fe7b1ab54f1c
Alessio <ale.privuse@gmail.com>
2026-06-16 18:46:04 +0200

    Update twiceAsOld-solution.js

--- Exercises/twice old/twiceAsOld-solution.js
+++ Exercises/twice old/twiceAsOld-solution.js
@@ -1,14 +1,2 @@
-
 function twiceAsOld(dadYearsOld, sonYearsOld) {
-  let time= 0;
-  while(true){
-    if(dadYearsOld == sonYearsOld * 2){
-      return time
-    }
-    else{
-     dadYearsOld = dadYearsOld + 1
-    sonYearsOld = sonYearsOld + 1
-      time++
-    }
-  }
-}
+  return Math.abs(dadYearsOld - 2 * sonYearsOld);
